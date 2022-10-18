0f7da70f69b9843a882e837157f54f34fc5f7b07
Maharani Nirmala Dewi <maharanisakti015@gmail.com>
2022-10-18 16:19:59 +0700

    halaman tambah staff

---
+++ app/src/main/java/com/group4/ecommerce/admin/AddStaffActivity.java
@@ -0,0 +1,16 @@
+package com.group4.ecommerce.admin;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.group4.ecommerce.R;
+
+public class AddStaffActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_staff);
+    }
+}

b4ddefcac4b7228147c5310d93aa398b00ac0fc2
Maharani Nirmala Dewi <maharanisakti015@gmail.com>
2022-10-18 16:19:23 +0700

    halaman admin

---
+++ app/src/main/java/com/group4/ecommerce/admin/AdminActivity.java
@@ -0,0 +1,65 @@
+package com.group4.ecommerce.admin;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.group4.ecommerce.R;
+import com.group4.ecommerce.WelcomeActivity;
+import com.group4.ecommerce.databinding.ActivityAdminBinding;
+
+public class AdminActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
+    private ActivityAdminBinding binding;
+    FirebaseDatabase database= FirebaseDatabase.getInstance();
+    FirebaseAuth auth=FirebaseAuth.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityAdminBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.appBarAdmin.toolbar);
+
+        DrawerLayout drawer = binding.drawerLayout;
+        NavigationView navigationView = binding.navView;
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_staff,R.id.nav_stock)
+                .setOpenableLayout(drawer)
+                .build();
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_admin);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+        navigationView.getMenu().findItem(R.id.nav_signout).setOnMenuItemClickListener(menuItem -> {
+            FirebaseAuth.getInstance().signOut();
+            Intent i= new Intent(AdminActivity.this, WelcomeActivity.class);
+            startActivity(i);
+            return true;
+        });
+//        navigationView.getMenu().findItem(R.id.nav_host_fragment_content_admin)
+    }
+
+
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_admin);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+}

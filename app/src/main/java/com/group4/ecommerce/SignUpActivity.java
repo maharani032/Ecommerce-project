dddbb3abbc3af37c70e1c5edf05cd899101db912
Maharani Nirmala Dewi <maharanisakti015@gmail.com>
2022-10-18 23:03:40 +0700

    user signup

--- app/src/main/java/com/group4/ecommerce/SignUpActivity.java
+++ app/src/main/java/com/group4/ecommerce/SignUpActivity.java
@@ -1,11 +1,11 @@
 package com.group4.ecommerce;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -36,23 +36,26 @@ public class SignUpActivity extends AppCompatActivity {
 //        signup("admin1@gmail.com","123456","admin1");
     }
 
-    public void signup(String email,String password, String username){
+    public void signup(String email,String password, String fullname){
+//        register untuk user
         auth.createUserWithEmailAndPassword(email,password)
                 .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()){
                             reference.child("Users").child(auth.getUid()).child("email").setValue(email);
-                            reference.child("Users").child(auth.getUid()).child("username").setValue(username);
+
 //                        role=> user,staff,admin
                             reference.child("Users").child(auth.getUid()).child("admin").setValue(false);
                             reference.child("Users").child(auth.getUid()).child("staff").setValue(false);
-//                            jika admin == true maka membuat database baru
-//                            reference.child("Admins").child(auth.getUid()).child("email").setValue(email);
+
+                            reference.child("User").child(auth.getUid()).child("Fullname").setValue(fullname);
+                            reference.child("User").child(auth.getUid()).child("email").setValue(email);
+
                             Intent welcome=new Intent(SignUpActivity.this,WelcomeActivity.class);
                             startActivity(welcome);
                         }else if(!task.isSuccessful()){
-                            Toast.makeText(SignUpActivity.this,"check email dan password",Toast.LENGTH_LONG).show();
+                            Toast.makeText(SignUpActivity.this,"cek email dan password",Toast.LENGTH_LONG).show();
                         }
 
                     }

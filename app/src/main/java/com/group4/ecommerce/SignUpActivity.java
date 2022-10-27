8f0d7f7d76dd6291af409066c4355ceac37623de
Maharani Nirmala Dewi <maharanisakti015@gmail.com>
2022-10-27 23:53:42 +0700

    halaman register user

--- app/src/main/java/com/group4/ecommerce/SignUpActivity.java
+++ app/src/main/java/com/group4/ecommerce/SignUpActivity.java
@@ -2,59 +2,99 @@ package com.group4.ecommerce;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.group4.ecommerce.model.Auth;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 public class SignUpActivity extends AppCompatActivity {
-    FirebaseAuth auth;
     FirebaseDatabase database;
     DatabaseReference reference;
     FirebaseStorage firebaseStorage;
     StorageReference storageReference;
+    Button bSignUp;
+    EditText email,password,phone;
+    boolean checkEmail;
+    List<String> lemail= new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        auth= FirebaseAuth.getInstance();
         database=FirebaseDatabase.getInstance();
         reference=database.getReference();
         firebaseStorage=FirebaseStorage.getInstance();
         storageReference=firebaseStorage.getReference();
-//create data manual
-//        signup("admin1@gmail.com","123456","admin1");
+
+        bSignUp=findViewById(R.id.bSignUp);
+        email=findViewById(R.id.input_email);
+        password=findViewById(R.id.input_password);
+        phone=findViewById(R.id.input_phone);
+
+        bSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String mail=email.getText().toString().replaceAll("\\s+","");;
+                String pw=password.getText().toString();
+                String ph=phone.getText().toString();
+
+                if(!mail.isEmpty() && !pw.isEmpty()&& !ph.isEmpty()) signup(mail,pw,ph);
+                else if(password.length()<=5) Toast.makeText(SignUpActivity.this,"Minimum length of password",Toast.LENGTH_SHORT).show();
+                else Toast.makeText(SignUpActivity.this,"Please Input Your Email and Password",Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
     }
 
-    public void signup(String email,String password, String fullname){
+    public void signup(String email,String password, String phone){
+
 //        register untuk user
-        auth.createUserWithEmailAndPassword(email,password)
-                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()){
-                            reference.child("Users").child(auth.getUid()).child("email").setValue(email);
-//                        role=> user,staff,admin
-                            reference.child("Users").child(auth.getUid()).child("role").setValue("user");
-                            reference.child("Users").child(auth.getUid()).child("Fullname").setValue(fullname);
+        reference.child("Auth").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot keys:snapshot.getChildren()){
 
-                            Intent welcome=new Intent(SignUpActivity.this,WelcomeActivity.class);
-                            startActivity(welcome);
-                        }else if(!task.isSuccessful()){
-                            Toast.makeText(SignUpActivity.this,"cek email dan password",Toast.LENGTH_LONG).show();
-                        }
+                    Auth auth=keys.getValue(Auth.class);
+                    lemail.add(auth.getEmail().toString());
+                }
+                Log.i("auth",lemail.toString());
+                if(!lemail.contains(email)){
+                    UUID randomId= UUID.randomUUID();
+                    reference.child("Auth").child(randomId.toString()).child("id").setValue(randomId.toString());
+                    reference.child("Auth").child(randomId.toString()).child("email").setValue(email);
+                    reference.child("Auth").child(randomId.toString()).child("password").setValue(password);
+                    reference.child("Auth").child(randomId.toString()).child("phone").setValue(phone);
+                    reference.child("Auth").child(randomId.toString()).child("role").setValue("user");
+                    Intent i= new Intent(SignUpActivity.this,WelcomeActivity.class);
+                    startActivity(i);
+                }else{
+                    Toast.makeText(SignUpActivity.this,"email sudah terpakai.",Toast.LENGTH_LONG).show();
+                    return;
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
 
-                    }
-                });
+            }
+        });
+
     }
 }

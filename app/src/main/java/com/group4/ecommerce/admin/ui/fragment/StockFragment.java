439fd0f3e302e844e0bbf015ce367990b26d6da9
Maharani Nirmala Dewi <maharanisakti015@gmail.com>
2022-10-28 18:45:49 +0700

    product

--- app/src/main/java/com/group4/ecommerce/admin/ui/fragment/StockFragment.java
+++ app/src/main/java/com/group4/ecommerce/admin/ui/fragment/StockFragment.java
@@ -1,28 +1,81 @@
 package com.group4.ecommerce.admin.ui.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.group4.ecommerce.admin.AddProductActivity;
 import com.group4.ecommerce.databinding.FragmentStockBinding;
 
 public class StockFragment extends Fragment {
-
+    ActivityResultLauncher<Intent> activityResultLauncherForAddProduct;
     private FragmentStockBinding binding;
+    RecyclerView rv;
 
+    DatabaseReference reference;
+    FirebaseDatabase database;
+
+    FloatingActionButton fabProduct;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         binding = FragmentStockBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textSlideshow;
+        fabProduct=binding.fabProduct;
+        fabProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent addProduct=new Intent(getActivity(), AddProductActivity.class);
+                activityResultLauncherForAddProduct.launch(addProduct);
+            }
+        });
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        RegisterActivityForAddProduct();
+
+    }
+
+    public void RegisterActivityForAddProduct() {
+        activityResultLauncherForAddProduct= registerForActivityResult(new ActivityResultContracts.StartActivityForResult()
+                , new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        int ResultCode=result.getResultCode();
+                        Intent data=result.getData();
+//                        if(ResultCode==RESULT_OK && data !=null){
+//                            Log.i("data product fragment",data.toString());
+//                            String id=data.getStringExtra("id");
+//                            String fullname=data.getStringExtra("fullname");
+//                            String image=data.getStringExtra("image");
+//                            String email=data.getStringExtra("email");
+//                            if(id!=null && fullname!=null && image!=null && email!=null){
+//                                reference.child("Staffs").child(id).child("fullname").setValue(fullname);
+//                                reference.child("Staffs").child(id).child("image").setValue(image);
+//                                reference.child("Staffs").child(id).child("email").setValue(email);
+//                                notify();
+//                            }
+//                        }
+                    }
+                });
     }
 
     @Override
---
+++ app/src/main/java/com/group4/ecommerce/model/product.java
@@ -0,0 +1,82 @@
+package com.group4.ecommerce.model;
+
+public class product {
+    String id,image,name,jumlah,harga,filter,kategori,description;
+    public product(){
+
+    }
+    public product(String id, String image, String name, String jumlah, String harga, String filter, String kategori, String description) {
+        this.id = id;
+        this.image = image;
+        this.name = name;
+        this.jumlah = jumlah;
+        this.harga = harga;
+        this.filter = filter;
+        this.kategori = kategori;
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getJumlah() {
+        return jumlah;
+    }
+
+    public void setJumlah(String jumlah) {
+        this.jumlah = jumlah;
+    }
+
+    public String getHarga() {
+        return harga;
+    }
+
+    public void setHarga(String harga) {
+        this.harga = harga;
+    }
+
+    public String getFilter() {
+        return filter;
+    }
+
+    public void setFilter(String filter) {
+        this.filter = filter;
+    }
+
+    public String getKategori() {
+        return kategori;
+    }
+
+    public void setKategori(String kategori) {
+        this.kategori = kategori;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
